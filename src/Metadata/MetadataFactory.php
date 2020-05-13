128f3c101719cdc7cc862655608cae2a732b7f60
Lasicka <tomas.benedikt@gmail.com>
2020-05-13 14:18:32 +0200

    style: codesniffer

--- src/Metadata/MetadataFactory.php
+++ src/Metadata/MetadataFactory.php
@@ -4,7 +4,6 @@ declare(strict_types=1);
 
 namespace JSONAPI\Metadata;
 
-
 use JSONAPI\Driver\Driver;
 use JSONAPI\Exception\Driver\ClassNotExist;
 use JSONAPI\Exception\Driver\ClassNotResource;
--- src/Uri/Sorting/SortParser.php
+++ src/Uri/Sorting/SortParser.php
@@ -4,7 +4,6 @@ declare(strict_types=1);
 
 namespace JSONAPI\Uri\Sorting;
 
-
 /**
  * Class SortParser
  *
