698b7f85e5e7562eba5454b647f63d97e6c2190c
Lasicka <tomas.benedikt@gmail.com>
2020-09-16 22:28:20 +0200

    chore: remove php stan because using nette

---
+++ tests-resources/JSONTestObject.php
@@ -0,0 +1,30 @@
+<?php
+
+declare(strict_types=1);
+
+namespace JSONAPI\Test\Resources;
+
+use JSONAPI\Document\Convertible;
+
+class JSONTestObject implements Convertible
+{
+
+    private string $key = 'value';
+
+
+    public static function jsonDeserialize($json): self
+    {
+        $instance = new self();
+        if (is_array($json)) {
+            $instance->key = $json['key'];
+        } else {
+            $instance->key = $json->key;
+        }
+        return $instance;
+    }
+
+    public function jsonSerialize()
+    {
+        return ['key' => $this->key];
+    }
+}
---
+++ tests/Document/ConvertibleTest.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace JSONAPI\Test\Document;
+
+use JSONAPI\Test\Resources\JSONTestObject;
+use PHPUnit\Framework\TestCase;
+
+class ConvertibleTest extends TestCase
+{
+
+    public function testJsonSerialize()
+    {
+        $obj = new JSONTestObject();
+
+        $this->assertEquals(['key' => 'value'], $obj->jsonSerialize());
+        $this->assertEquals('{"key":"value"}', json_encode($obj));
+    }
+
+    public function testJsonDeserialize()
+    {
+        $stdTest   = json_decode(json_encode(['key' => 'value']), false);
+        $arrayTest = json_decode(json_encode(['key' => 'value']), true);
+
+        $obj = JSONTestObject::jsonDeserialize($stdTest);
+        $this->assertInstanceOf(JSONTestObject::class, $obj);
+        $obj = JSONTestObject::jsonDeserialize($arrayTest);
+        $this->assertInstanceOf(JSONTestObject::class, $obj);
+    }
+}
