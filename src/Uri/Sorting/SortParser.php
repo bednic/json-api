fad6240842001bcb5d96bd200dd84229be57627b
Lasicka <tomas.benedikt@gmail.com>
2020-11-12 22:22:18 +0100

    fix(SortParser::parse incompatible behavior): clrf
    Fix parse method, to throw bad request if sort parameter is malformed
    
    Closes #33

--- src/Uri/Sorting/SortParser.php
+++ src/Uri/Sorting/SortParser.php
@@ -4,6 +4,9 @@ declare(strict_types=1);
 
 namespace JSONAPI\Uri\Sorting;
 
+use JSONAPI\Exception\Http\MalformedParameter;
+use JSONAPI\Uri\QueryPartInterface;
+
 /**
  * Class SortParser
  *
@@ -20,14 +23,18 @@ class SortParser implements SortInterface
      * @param string|null $data
      *
      * @return SortInterface
+     * @throws MalformedParameter
      */
     public function parse(?string $data): SortInterface
     {
         $this->sort = [];
         if ($data && strlen($data) > 0) {
-            preg_match_all('/((?P<sort>-?)(?P<field>[a-zA-Z0-9.]+))/', $data, $matches);
-            foreach ($matches['field'] as $i => $field) {
-                $this->sort[$field] = $matches['sort'][$i] ? SortInterface::DESC : SortInterface::ASC;
+            if (preg_match_all('/((?P<sort>-?)(?P<field>[a-zA-Z0-9.]+))/', $data, $matches) !== false) {
+                foreach ($matches['field'] as $i => $field) {
+                    $this->sort[$field] = $matches['sort'][$i] ? SortInterface::DESC : SortInterface::ASC;
+                }
+            } else {
+                throw new MalformedParameter(QueryPartInterface::SORT_PART_KEY);
             }
         }
         return $this;
